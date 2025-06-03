4b39ad40961df6cc33f4f0b4c197251fafa4d8b3
Jay chinche <94739010+jaychinche@users.noreply.github.com>
2025-06-03 17:28:06 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,7 +44,7 @@ exports.login = async (req, res, next) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' },
+      { expiresIn: '4d' },
 
     );
     res.status(200).json({
